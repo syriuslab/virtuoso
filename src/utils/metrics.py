809c4e560c06e55245bb61894e09753dd6c0c323
syriuslab <syriuscloudarchitect@gmail.com>
2024-08-23 14:09:12 +0200

    upload_full_virtuoso_to_repo

---
+++ src/utils/metrics.py
@@ -0,0 +1,9 @@
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+def evaluate_model(y_true, y_pred):
+    return {
+        'accuracy': accuracy_score(y_true, y_pred),
+        'precision': precision_score(y_true, y_pred, average='weighted'),
+        'recall': recall_score(y_true, y_pred, average='weighted'),
+        'f1': f1_score(y_true, y_pred, average='weighted')
+    }
